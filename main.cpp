a521f4f0c01b3167330da6bc875da1c249c4877e
Nek-12 <vaizin.nikita@gmail.com>
2020-05-07 14:05:07 +0300

    improved syntax, changed gitignore, changed project name

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <set>
 #include <fstream>
-using ull = unsigned long long;
 
 int main(int argc, char *argv[])
 {
@@ -11,31 +10,27 @@ int main(int argc, char *argv[])
         path = argv[1];
     else
     {
-        path = "";
         std::cout << "Hello, type or drag a text file into a window to remove duplicate lines" << std::endl;
         std::getline(std::cin, path);
     }
     try
     {
         std::cout << "The path is " << path << std::endl;
-        std::cout << "Please stand by" << std::endl;
-
-        std::ifstream input(path);
-        if (!input) throw std::runtime_error("Error opening input file.");
-
-        ull cnt = 0;
+        std::cout << "Please stand by..." << std::endl;
+        std::ifstream f(path);
+        if (!f) throw std::runtime_error("Error opening input file.");
+        size_t cnt = 0;
         std::ofstream output(path + "_dedup");
         if (!output) throw std::runtime_error("Error creating output file.");
-
         std::set<std::string> set;
-        while (input)
+        while (f)
         {
             std::string line;
-            std::getline(input, line);
+            std::getline(f, line);
             set.insert(line);
             ++cnt;
             if (cnt > (set.max_size() - 1000)) throw std::runtime_error("Too large file.");
-            if (cnt % 5000 == 0)
+            if (cnt % 5000 == 0) //every 5000th line, output a message
                 std::cout << "Processed " << cnt << " elements" << std::endl;
         }
         if (cnt < 2) throw std::invalid_argument("No data?");
@@ -43,20 +38,20 @@ int main(int argc, char *argv[])
         for (auto& el: set)
             output << el << "\n";
         std::cout << "Processed " << cnt
-        << " elements. Removed " << cnt - set.size()  << " lines." << std::endl;
+                  << " elements. Removed " << cnt - set.size()  << " lines." << std::endl;
         system("pause");
-        return 0;
+        return EXIT_SUCCESS;
     }
     catch (const std::exception& exc)
     {
         std::cerr << exc.what() << std::endl;
         system("pause");
-        return (-1);
+        return (EXIT_FAILURE);
     }
     catch (...)
     {
-        std::cerr << "Unknown error. Restarting." << std::endl;
+        std::cerr << "Unknown error. Unable to continue." << std::endl;
         system("pause");
-        return(-1);
+        return(EXIT_FAILURE);
     }
 }
