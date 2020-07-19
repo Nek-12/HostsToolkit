48bd928911e3d4c1b223ec0caf12a2358f19952b
Nek-12 <vaizin.nikita@gmail.com>
2020-07-19 14:36:18 +0300

    some tweaks  to cmake to attempt to build statically

--- HT_ru_RU.ts
+++ HT_ru_RU.ts
@@ -5,93 +5,86 @@
     <name>MainWindow</name>
     <message>
         <location filename="mainwindow.ui" line="35"/>
-        <location filename="cmake-build-debug/HT_autogen/include/ui_mainwindow.h" line="174"/>
-        <location filename="cmake-build-minsizerel/HT_autogen/include/ui_mainwindow.h" line="174"/>
+        <location filename="cmake-build-minsizerel/HT_autogen/include/ui_mainwindow.h" line="178"/>
         <source>HostsToolkit</source>
         <extracomment>Console</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="98"/>
-        <location filename="cmake-build-debug/HT_autogen/include/ui_mainwindow.h" line="176"/>
-        <location filename="cmake-build-minsizerel/HT_autogen/include/ui_mainwindow.h" line="176"/>
+        <location filename="cmake-build-minsizerel/HT_autogen/include/ui_mainwindow.h" line="180"/>
         <source> Add a custom entry here...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="123"/>
-        <location filename="cmake-build-debug/HT_autogen/include/ui_mainwindow.h" line="177"/>
-        <location filename="cmake-build-minsizerel/HT_autogen/include/ui_mainwindow.h" line="177"/>
+        <location filename="cmake-build-minsizerel/HT_autogen/include/ui_mainwindow.h" line="181"/>
         <source>Add</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="148"/>
-        <location filename="cmake-build-debug/HT_autogen/include/ui_mainwindow.h" line="178"/>
-        <location filename="cmake-build-minsizerel/HT_autogen/include/ui_mainwindow.h" line="178"/>
+        <location filename="cmake-build-minsizerel/HT_autogen/include/ui_mainwindow.h" line="182"/>
         <source>Apply to System</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="188"/>
-        <location filename="cmake-build-debug/HT_autogen/include/ui_mainwindow.h" line="179"/>
-        <location filename="cmake-build-minsizerel/HT_autogen/include/ui_mainwindow.h" line="179"/>
+        <location filename="cmake-build-minsizerel/HT_autogen/include/ui_mainwindow.h" line="183"/>
         <source>Save...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="213"/>
-        <location filename="cmake-build-debug/HT_autogen/include/ui_mainwindow.h" line="180"/>
-        <location filename="cmake-build-minsizerel/HT_autogen/include/ui_mainwindow.h" line="180"/>
+        <location filename="cmake-build-minsizerel/HT_autogen/include/ui_mainwindow.h" line="184"/>
         <source>Load system hosts</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="267"/>
-        <location filename="cmake-build-debug/HT_autogen/include/ui_mainwindow.h" line="183"/>
-        <location filename="cmake-build-minsizerel/HT_autogen/include/ui_mainwindow.h" line="183"/>
+        <location filename="cmake-build-minsizerel/HT_autogen/include/ui_mainwindow.h" line="187"/>
         <source>Remove duplicates + sort</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="280"/>
-        <location filename="cmake-build-debug/HT_autogen/include/ui_mainwindow.h" line="184"/>
-        <location filename="cmake-build-minsizerel/HT_autogen/include/ui_mainwindow.h" line="184"/>
+        <location filename="cmake-build-minsizerel/HT_autogen/include/ui_mainwindow.h" line="188"/>
         <source>Add credits and statistics</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="343"/>
-        <location filename="cmake-build-debug/HT_autogen/include/ui_mainwindow.h" line="185"/>
-        <location filename="cmake-build-minsizerel/HT_autogen/include/ui_mainwindow.h" line="185"/>
-        <source>Files</source>
+        <location filename="cmake-build-minsizerel/HT_autogen/include/ui_mainwindow.h" line="189"/>
+        <source>Files to merge:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="388"/>
+        <location filename="cmake-build-minsizerel/HT_autogen/include/ui_mainwindow.h" line="192"/>
+        <source>About</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="359"/>
-        <location filename="cmake-build-debug/HT_autogen/include/ui_mainwindow.h" line="186"/>
-        <location filename="cmake-build-minsizerel/HT_autogen/include/ui_mainwindow.h" line="186"/>
+        <location filename="cmake-build-minsizerel/HT_autogen/include/ui_mainwindow.h" line="190"/>
         <source>Custom Entries</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="375"/>
-        <location filename="cmake-build-debug/HT_autogen/include/ui_mainwindow.h" line="187"/>
-        <location filename="cmake-build-minsizerel/HT_autogen/include/ui_mainwindow.h" line="187"/>
+        <location filename="cmake-build-minsizerel/HT_autogen/include/ui_mainwindow.h" line="191"/>
         <source>Delete</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="254"/>
-        <location filename="cmake-build-debug/HT_autogen/include/ui_mainwindow.h" line="182"/>
-        <location filename="cmake-build-minsizerel/HT_autogen/include/ui_mainwindow.h" line="182"/>
+        <location filename="cmake-build-minsizerel/HT_autogen/include/ui_mainwindow.h" line="186"/>
         <source>Remove comments</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="241"/>
-        <location filename="cmake-build-debug/HT_autogen/include/ui_mainwindow.h" line="181"/>
-        <location filename="cmake-build-minsizerel/HT_autogen/include/ui_mainwindow.h" line="181"/>
+        <location filename="cmake-build-minsizerel/HT_autogen/include/ui_mainwindow.h" line="185"/>
         <source>Browse for a file...</source>
         <translation type="unfinished"></translation>
     </message>
