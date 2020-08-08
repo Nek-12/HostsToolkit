481665b8b431c716482f138bf28ecd76f4bff62d
Nek-12 <vaizin.nikita@gmail.com>
2020-08-08 17:36:43 +0300

    improved ui layout, fixed typos, fixed screenshots

--- HT_ru_RU.ts
+++ HT_ru_RU.ts
@@ -43,105 +43,106 @@ t.me/Nek_12
     <name>MainWindow</name>
     <message>
         <location filename="src/app.ui" line="35"/>
-        <location filename="build/HT_autogen/include/ui_app.h" line="238"/>
-        <source>HostsToolkit</source>
+        <location filename="build/HT_autogen/include/ui_app.h" line="233"/>
+        <source>HostsTools</source>
+        <oldsource>HostsToolkit</oldsource>
         <extracomment>Console</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="src/app.ui" line="63"/>
-        <location filename="build/HT_autogen/include/ui_app.h" line="252"/>
+        <location filename="build/HT_autogen/include/ui_app.h" line="247"/>
         <source>Overview</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="src/app.ui" line="127"/>
-        <location filename="build/HT_autogen/include/ui_app.h" line="240"/>
+        <location filename="build/HT_autogen/include/ui_app.h" line="235"/>
         <source> Add a custom entry here...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="src/app.ui" line="220"/>
-        <location filename="build/HT_autogen/include/ui_app.h" line="246"/>
+        <location filename="build/HT_autogen/include/ui_app.h" line="241"/>
         <source>Add</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="src/app.ui" line="336"/>
-        <location filename="build/HT_autogen/include/ui_app.h" line="251"/>
+        <location filename="build/HT_autogen/include/ui_app.h" line="246"/>
         <source>Calculate statistics</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="src/app.ui" line="355"/>
-        <location filename="build/HT_autogen/include/ui_app.h" line="260"/>
+        <location filename="build/HT_autogen/include/ui_app.h" line="255"/>
         <source>Hosts Sources</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="src/app.ui" line="389"/>
-        <location filename="build/HT_autogen/include/ui_app.h" line="253"/>
+        <location filename="build/HT_autogen/include/ui_app.h" line="248"/>
         <source>Browse...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="src/app.ui" line="402"/>
-        <location filename="build/HT_autogen/include/ui_app.h" line="254"/>
+        <location filename="build/HT_autogen/include/ui_app.h" line="249"/>
         <source>Files</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="src/app.ui" line="441"/>
-        <location filename="build/HT_autogen/include/ui_app.h" line="256"/>
+        <location filename="build/HT_autogen/include/ui_app.h" line="251"/>
         <source>Network URLs</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="src/app.ui" line="285"/>
-        <location filename="build/HT_autogen/include/ui_app.h" line="248"/>
+        <location filename="build/HT_autogen/include/ui_app.h" line="243"/>
         <source>Save...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="src/app.ui" line="476"/>
-        <location filename="build/HT_autogen/include/ui_app.h" line="258"/>
+        <location filename="build/HT_autogen/include/ui_app.h" line="253"/>
         <source>Add...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="src/app.ui" line="508"/>
-        <location filename="build/HT_autogen/include/ui_app.h" line="259"/>
+        <location filename="build/HT_autogen/include/ui_app.h" line="254"/>
         <source>Load system</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="src/app.ui" line="159"/>
-        <location filename="build/HT_autogen/include/ui_app.h" line="242"/>
+        <location filename="build/HT_autogen/include/ui_app.h" line="237"/>
         <source>Remove duplicates + sort</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="src/app.ui" line="172"/>
-        <location filename="build/HT_autogen/include/ui_app.h" line="243"/>
+        <location filename="build/HT_autogen/include/ui_app.h" line="238"/>
         <source>Add credits</source>
         <oldsource>Add credits and statistics</oldsource>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="src/app.ui" line="201"/>
-        <location filename="build/HT_autogen/include/ui_app.h" line="245"/>
+        <location filename="build/HT_autogen/include/ui_app.h" line="240"/>
         <source>About</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="src/app.ui" line="245"/>
-        <location filename="build/HT_autogen/include/ui_app.h" line="247"/>
+        <location filename="build/HT_autogen/include/ui_app.h" line="242"/>
         <source>Apply to System!</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="src/app.ui" line="304"/>
-        <location filename="build/HT_autogen/include/ui_app.h" line="249"/>
+        <location filename="build/HT_autogen/include/ui_app.h" line="244"/>
         <source>Custom Entries</source>
         <translation type="unfinished"></translation>
     </message>
@@ -149,15 +150,15 @@ t.me/Nek_12
         <location filename="src/app.ui" line="188"/>
         <location filename="src/app.ui" line="418"/>
         <location filename="src/app.ui" line="457"/>
-        <location filename="build/HT_autogen/include/ui_app.h" line="244"/>
-        <location filename="build/HT_autogen/include/ui_app.h" line="255"/>
-        <location filename="build/HT_autogen/include/ui_app.h" line="257"/>
+        <location filename="build/HT_autogen/include/ui_app.h" line="239"/>
+        <location filename="build/HT_autogen/include/ui_app.h" line="250"/>
+        <location filename="build/HT_autogen/include/ui_app.h" line="252"/>
         <source>Delete</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="src/app.ui" line="146"/>
-        <location filename="build/HT_autogen/include/ui_app.h" line="241"/>
+        <location filename="build/HT_autogen/include/ui_app.h" line="236"/>
         <source>Remove comments</source>
         <translation type="unfinished"></translation>
     </message>
